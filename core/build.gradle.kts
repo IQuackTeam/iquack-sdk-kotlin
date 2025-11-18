e40582a28ca4f654f76138b2f468cb4091ba6f46
duckrya <foxlitegor@gmail.com>
2025-11-18 19:33:36 +0300

    add targets in multiplatform

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,57 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.jetbrains.kotlin.multiplatform)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.android.library)
+//    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "ru.iquack.sdk"
+    compileSdk = 36
+    defaultConfig {
+        minSdk = 26
+    }
+}
+
+kotlin {
+    androidTarget{
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+//    iosX64()
+//    iosArm64()
+//    iosSimulatorArm64()
+
+    jvm()
+    js {
+        browser()
+        binaries.executable()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.cio)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.kotlinx.serialization.json)
+            }
+        }
+    }
 }
 
 dependencies {
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio) // JVM engine
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.kotlinx.serialization.json)
+
 }

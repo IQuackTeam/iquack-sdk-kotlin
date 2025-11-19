d4f5e5d9471d499409e5529f285479ff31a4eb00
duckrya <foxlitegor@gmail.com>
2025-11-19 23:35:17 +0300

    Release v1.0.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.multiplatform)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.android.library)
-    alias(libs.plugins.maven.publish)
+    id("maven-publish")
 }
 
 android {
@@ -14,9 +14,6 @@ android {
     defaultConfig {
         minSdk = 26
     }
-//    publishing {
-//        singleVariant("release")
-//    }
 }
 
 kotlin {
@@ -43,14 +40,12 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(libs.ktor.client.core)
-                implementation(libs.ktor.client.cio)
-                implementation(libs.ktor.client.content.negotiation)
-                implementation(libs.ktor.serialization.kotlinx.json)
-                implementation(libs.kotlinx.serialization.json)
-            }
+        commonMain.dependencies {
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.serialization.json)
         }
     }
 }
@@ -61,35 +56,34 @@ dependencies {
 group = findProperty("GROUP") as String
 version = findProperty("VERSION_NAME") as String
 
-mavenPublishing {
-    pom {
-        name.set("IQuack SDK Core")
-        description.set(findProperty("POM_DESCRIPTION") as String)
-        url.set(findProperty("POM_URL") as String)
-        licenses {
-            license {
-                name.set(findProperty("POM_LICENSE_NAME") as String)
-                url.set(findProperty("POM_LICENSE_URL") as String)
-                distribution.set(findProperty("POM_LICENSE_DIST") as String)
-            }
-        }
-        scm {
-            url.set(findProperty("POM_SCM_URL") as String)
-            connection.set(findProperty("POM_SCM_CONNECTION") as String)
-            developerConnection.set(findProperty("POM_SCM_DEV_CONNECTION") as String)
-        }
-        developers {
-            developer {
-                id.set(findProperty("POM_DEVELOPER_ID") as String)
-                name.set(findProperty("POM_DEVELOPER_NAME") as String)
-                url.set(findProperty("POM_DEVELOPER_URL") as String)
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+            pom {
+                name.set("IQuack SDK Core")
+                description.set(findProperty("POM_DESCRIPTION") as String)
+                url.set(findProperty("POM_URL") as String)
+                licenses {
+                    license {
+                        name.set(findProperty("POM_LICENSE_NAME") as String)
+                        url.set(findProperty("POM_LICENSE_URL") as String)
+                        distribution.set(findProperty("POM_LICENSE_DIST") as String)
+                    }
+                }
+                scm {
+                    url.set(findProperty("POM_SCM_URL") as String)
+                    connection.set(findProperty("POM_SCM_CONNECTION") as String)
+                    developerConnection.set(findProperty("POM_SCM_DEV_CONNECTION") as String)
+                }
+                developers {
+                    developer {
+                        id.set(findProperty("POM_DEVELOPER_ID") as String)
+                        name.set(findProperty("POM_DEVELOPER_NAME") as String)
+                        url.set(findProperty("POM_DEVELOPER_URL") as String)
+                    }
+                }
             }
         }
     }
-
-//    // Configure publishing to Maven Central
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-//
-//    // Enable GPG signing for all publications
-//    signAllPublications()
 }
